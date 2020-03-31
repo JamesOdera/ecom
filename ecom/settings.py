e142a40c000264f536fe69c44a8b1b2488f7c92d
JamesOdera <nyangoma.odera@gmail.com>
2020-03-31 11:38:33 +0300

    Create the users app for the user authentication and then create a register form for user registration

--- ecom/settings.py
+++ ecom/settings.py
@@ -33,6 +33,8 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'bizz',
     'bootstrap3',
+    'users.apps.UsersConfig',
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
