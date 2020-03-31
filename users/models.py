d7574405bc8fab106c03e9bd35fca0e2bfb8e07f
JamesOdera <nyangoma.odera@gmail.com>
2020-03-31 14:36:03 +0300

    create a login html for users to login to the app

--- users/models.py
+++ users/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length =60)
+    firstname = models.CharField(max_length =60)
+    lastname = models.CharField(max_length =60)
+    email = models.EmailField()
+    userType = models.CharField(max_length =60)
+    status = models.CharField(max_length =60)
+    pub_date = models.DateTimeField(auto_now_add=True)
