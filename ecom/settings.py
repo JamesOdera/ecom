f7356fef11ebbac9f6822a3361f2ecf5a41a7815
JamesOdera <nyangoma.odera@gmail.com>
2020-03-31 15:35:57 +0300

    add users profile for the users to have their profile page

--- ecom/settings.py
+++ ecom/settings.py
@@ -128,6 +128,9 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 CRISPY_TEMPLATE_PACK = 'bootstrap3'
 
 LOGIN_REDIRECT_URL = 'index'
--- users/models.py
+++ users/models.py
@@ -1,10 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
+from PIL import Image
 
-class User(models.Model):
-    username = models.CharField(max_length =60)
-    firstname = models.CharField(max_length =60)
-    lastname = models.CharField(max_length =60)
-    email = models.EmailField()
-    userType = models.CharField(max_length =60)
-    status = models.CharField(max_length =60)
-    pub_date = models.DateTimeField(auto_now_add=True)
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
+    def save(self, *args, **kwargs):
+        super(Profile, self).save(*args, **kwargs)
+
+        img = Image.open(self.image.path)
+
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
+
