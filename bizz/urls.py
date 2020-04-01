9dc9a5bd0cea2e62a6b371fc35080728e0245e98
JamesOdera <nyangoma.odera@gmail.com>
2020-04-01 17:31:28 +0300

    add a detail page for the items posted for users to purchase

--- bizz/urls.py
+++ bizz/urls.py
@@ -2,10 +2,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
 from . import views
+from .views import PostListView, PostDetailView
 
 urlpatterns=[
     path('',views.index,name = 'index'),
-    path('product',views.product,name = 'product'),
+    path('product',PostListView.as_view(),name = 'product'),
+    path('post/<int:pk>',PostDetailView.as_view(), name = 'post-detail'),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- bizz/views.py
+++ bizz/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http  import HttpResponse
 from django.contrib.auth.decorators import login_required
-from .models import Post 
+from django.views.generic import ListView, DetailView 
+from .models import Post
+
 
 def index(request):
     return render(request, 'index.html')
@@ -12,3 +14,12 @@ def product(request):
         'posts': Post.objects.all()
     }
     return render(request, 'product.html', context)
+
+class PostListView(ListView):
+    model = Post
+    context_object_name = 'posts'
+    ordering = ['-pub_date']
+    template_name = 'product.html'
+
+class PostDetailView(DetailView):
+    model = Post
