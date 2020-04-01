f29315ec3a058af889ee78308255151fb4551450
JamesOdera <nyangoma.odera@gmail.com>
2020-04-01 18:50:21 +0300

    add the users ability to delete anything no wanted

--- bizz/urls.py
+++ bizz/urls.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
 from . import views
-from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
 
 urlpatterns=[
     path('',views.index,name = 'index'),
@@ -10,6 +10,7 @@ urlpatterns=[
     path('post/<int:pk>',PostDetailView.as_view(), name = 'post-detail'),
     path('post/new/',PostCreateView.as_view(),name = 'post-create'),
     path('post/<int:pk>/update/',PostUpdateView.as_view(), name = 'post-update'),
+    path('post/<int:pk>/delete/',PostDeleteView.as_view(), name = 'post-delete'),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- bizz/views.py
+++ bizz/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 from django.http  import HttpResponse
 from django.contrib.auth.decorators import login_required
-from django.views.generic import ListView, DetailView, CreateView, UpdateView 
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView 
 from .models import Post
-from django.contrib.auth.mixins import LoginRequiredMixin 
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin 
 
 
 def index(request):
@@ -33,10 +33,26 @@ class PostCreateView(LoginRequiredMixin, CreateView):
         form.instance.author = self.request.user
         return super().form_valid(form)
 
-class PostUpdateView(LoginRequiredMixin, UpdateView):
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
     fields = [ 'title', 'description', 'price', 'contact', 'category', 'location', 'cover']
 
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = '/product' 
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
