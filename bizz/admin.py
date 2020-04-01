5c3afed3003795a77c39b2a37140ad38d5e0aa8f
JamesOdera <nyangoma.odera@gmail.com>
2020-04-01 15:23:10 +0300

    add the category and the location on the admin page

--- bizz/admin.py
+++ bizz/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Image, Post
+from .models import Category,Location, Image, Post
 
+admin.site.register(Category)
+admin.site.register(Location)
 admin.site.register(Image)
 admin.site.register(Post)
 
