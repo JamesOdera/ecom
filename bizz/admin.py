67358f534b4ac3e61351e2f79b2307eb45218300
JamesOdera <nyangoma.odera@gmail.com>
2020-04-01 14:54:35 +0300

    add an Image model and a Post model for Users tom ost stuff they want to sell

--- bizz/admin.py
+++ bizz/admin.py
@@ -1,2 +1,6 @@
 from django.contrib import admin
+from .models import Image, Post
+
+admin.site.register(Image)
+admin.site.register(Post)
 
--- bizz/models.py
+++ bizz/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+import datetime as dt
+from django.contrib.auth.models import User
 
 
+class Category(models.Model):
+    name = models.CharField(max_length =30)
+
+    def __str__(self):
+        return self.name
+
+class Location(models.Model):
+    name = models.CharField(max_length =30)
+
+    def __str__(self):
+        return self.name
+
+class Image(models.Model):
+    image = models.ImageField(upload_to='images/')
+    name = models.CharField(max_length =60)
+    description = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    location = models.ForeignKey(Location, null=True, on_delete=models.CASCADE)
+
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()
+
+class Post(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User,  on_delete=models.CASCADE)
+    price =  models.CharField(max_length=50)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    location = models.ForeignKey(Location, null=True, on_delete=models.CASCADE)
+    cover = models.ImageField(upload_to='images/')
+
+    def __str__(self):
+        return self.title
